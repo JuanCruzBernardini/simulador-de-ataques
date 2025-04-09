fa9a0034f5a64ba8cd45c9e2dc5507cb71f09dd1
JuanCruzBernardini <108838661+JuanCruzBernardini@users.noreply.github.com>
2025-04-09 13:05:42 -0300

    Delete Scripts maliciosos directory

--- Scripts maliciosos/payloads modo vulnerable XSS.py
+++
@@ -1,5 +0,0 @@
-#<script>alert('XSS')</script>
-
-#<img src=x onerror=alert('XSS2')>
-
-#<svg onload=alert('SVG')>
--- Scripts maliciosos/payloads sqli.py
+++
@@ -1,5 +0,0 @@
-#' OR '1'='1 → lista todos los usuarios (si estuviera vulnerable)
-
- # admin' -- → intento de login o manipulación
-
-# ' OR 1=1 -- → prueba básica
